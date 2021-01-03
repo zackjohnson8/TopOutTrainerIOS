b892972209517906f29c72b5701a78c7d6a645fe
zackjohnson8 <zackjohnson8@gmail.com>
2021-01-03 12:28:16 -0800

    Initial Commit

---
+++ TopOutTrainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TopOutTrainer
+//
+//  Created by Zachary Johnson on 1/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
